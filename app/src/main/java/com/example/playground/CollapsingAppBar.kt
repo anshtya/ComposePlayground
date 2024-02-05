42f6e9f08b257677f77e47a384d012917029b504
Ansh Tyagi <tyagiansh486@gmail.com>
2024-02-05 22:16:24 +0530

    CollapsingAppBar

---
+++ app/src/main/java/com/example/playground/CollapsingAppBar.kt
@@ -0,0 +1,56 @@
+package com.example.playground
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LargeTopAppBar
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CollapsingAppBar() {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
+//    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior(rememberTopAppBarState())
+
+    Scaffold(
+        topBar = {
+            LargeTopAppBar(
+                title = { Text(text = "app title") },
+                scrollBehavior = scrollBehavior
+            )
+        },
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection)
+    ) { padding ->
+        Column(Modifier.padding(padding)) {
+            LazyColumn(Modifier.fillMaxSize()) {
+                items(
+                    items = (1..100).map { it },
+                    key = { it }
+                ) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier
+                            .size(100.dp)
+                            .fillMaxWidth()
+                    ) {
+                        Text(it.toString(), Modifier.padding(horizontal = 4.dp))
+                    }
+                }
+            }
+        }
+    }
+}
